dff93dde31b3354c8bb4781e35d4fa40497b7d02
trvon <trvon@protonmail.com>
2018-11-25 18:13:59 -0500

    Graphs complete

--- project/app.R
+++ project/app.R
@@ -4,7 +4,7 @@ server <- function(input, output) {
 	datasetInput <- reactive({
 		switch(input$dataset,
 			"Publisher" = Publisher,
-			"Genre" = yearRange(input$year, input$range),
+			"Genre" = yearRange(input$year, input$range, input$region),
 			# Selects table passed on table returned from function
 			"Rating" = ratingType(input$ratingsMethod, input$regionSales))
 	})
@@ -19,6 +19,15 @@ server <- function(input, output) {
 	output$view <- renderTable({
 		head(datasetInput(), n = input$obs)
 	})
+
+	output$plot <- renderPlot({
+		data <- datasetInput()
+		switch(input$dataset,
+			"Publisher" = { },
+			"Genre" = { plot(data$Genre) }, # , data[input$regionSales]) },
+			"Rating" = { plot(data$Sales, data$Combined_Rating)} # , data[input$ratingsMethod]) }
+		)
+	})
 }
 
 ui <- pageWithSidebar(
@@ -32,7 +41,7 @@ ui <- pageWithSidebar(
 		# Checks if Rating has been selected
 		conditionalPanel(
 			condition = "input.dataset == 'Rating'",
-			selectInput("ratingsMethod", "Scores", list("Critic", "User", "Combined")),
+			selectInput("ratingsMethod", "Scores", list("Critic", "User"),),
 			selectInput("regionSales", "Region Sales", list("NA", "EU", "JP", "Other"))
 		),
 		# Checks if Genre has been selected
@@ -41,12 +50,14 @@ ui <- pageWithSidebar(
 			sliderInput("year", "Year of Release:",
 				min = 1980, max = 2017, value = 1980),
 			sliderInput("range", "Year range:",
-				min = 1, max = 10, value = 5)
+				min = 1, max = 10, value = 5),
+			selectInput("region", "Region Sales", list("NA", "EU", "JP", "Other"))
 		)
 	),
 
 	mainPanel(
-		# verbatimTextOutput("summary"),
+		verbatimTextOutput("summary"),
+		plotOutput("plot"),
 		tableOutput("view")
 	)
 )
--- project/data.R
+++ project/data.R
@@ -16,9 +16,6 @@ lapply(packages, require, character.only = TRUE)
 # Loading Video Data
 VGdata <- read.csv("DATA/VideoGamesSales.csv", header=TRUE)
 
-# Summarize Black Friday data
-names(VGdata)
-
 ## Score Comparison
 # Normalizing Scores
 VGdata$Critic_Score <- as.character(VGdata$Critic_Score)
@@ -31,23 +28,16 @@ VGdata[is.na(VGdata)] <- 0
 # Changing values to numbers
 VGdata$Critic_Score <- as.numeric(VGdata$Critic_Score) / 10
 VGdata$User_Score <- as.numeric(VGdata$User_Score)
-# Combined Scores table
-CMdata <- VGdata["Name"]
-CMdata["Rating"] <- VGdata["Rating"]
-CMdata["Global_Sales"] <- VGdata["Global_Sales"]
-# Summing the average of the Critic and Users scores
-CMdata["Combined_Rating"] <- VGdata['Critic_Score'] + VGdata['User_Score']
-# Sorting data
-CMdata <- CMdata[order(CMdata$Combined_Rating, decreasing=TRUE), ]
-
 # Scores table
 Sdata <- VGdata["Name"]
 Sdata["Rating"] <- VGdata["Rating"]
+Sdata["Global_Sales"] <- as.numeric(as.character(VGdata["Global_Sales"]))
+# Combined Scores table
 
 # Region Specific
 # Changes year to numeric value
 VGdata$Year_of_Release <- as.numeric(as.character(VGdata$Year_of_Release))
-YVGdata <- VGdata
+YVGdata <- VGdata[order(VGdata$Year_of_Release),]
 
 #################
 # 	Functions 	#
@@ -61,32 +51,39 @@ ratingType <- function(rating, region) {
 		"JP" = { Sdata['Sales'] <- VGdata['JP_Sales'] },
 		"Other" = {	Sdata['Sales'] <- VGdata['Other_Sales'] }
 	)
+
+	Sdata['Sales'] <- as.numeric(as.character(Sdata$Sales))
 	# Return table with Critic ratings
 	if(rating == "Critic") {
 		Sdata["Critic_Score"] <- VGdata["Critic_Score"]
+		Sdata["Combined_Rating"] <- VGdata['Critic_Score'] + VGdata['User_Score']
 		# Returns table ordered by score
 		return(Sdata[order(Sdata$Critic_Score, decreasing=TRUE), ])
 	}
 	# Return table with User ratings	
 	else if (rating == "User") {
 		Sdata["User_Score"] <- VGdata["User_Score"]
+		Sdata["Combined_Rating"] <- VGdata['Critic_Score'] + VGdata['User_Score']
 		# Returns table ordered by score
 		return(Sdata[order(Sdata$User_Score, decreasing=TRUE), ])
-	}
-	# Return the combined scores
-	else {
-		return(CMdata)
 	}
 }
 
 # Function returns table based on year selection
-yearRange <- function(year, range) {
+yearRange <- function(year, range, region) {
 	# Creating a table with the year range passed
+	switch(region,
+		"NA" = { region <- "NA_Sales" },
+		"EU" = { region <- "EU_Sales" },
+		"JP" = { region <- "JP_Sales" },
+		"Other" = {	region <- "Other_Sales" }
+	)
 	max <- year + range
 	# Getting year subset in range selected
 	tempTable <- YVGdata[YVGdata$Year_of_Release >= year & YVGdata$Year_of_Release <= max,]
-	# initializing table
-	# Ydata <- null
-	Ydata <- count(tempTable, c('Year_of_Release', 'Genre'))
+	# Creating a table with the frequency of games and genre's
+	Ydata <- count(tempTable, c('Year_of_Release', 'Genre','Global_Sales', region))
+	# Aggregates the data based on similar year and Genre while summing other columns :D
+	Ydata <- aggregate(. ~ Year_of_Release + Genre + freq, Ydata, sum)
 	return(Ydata)
 }
